c66c512e00114ea11a6f902fc04b58fdf7fabb3f
Shaked Erinberg <shakede@wix.com>
2023-07-17 16:42:26 +0300

    chore(maintenance): upgrade dependencies

---
+++ pleb.config.mjs
@@ -0,0 +1,9 @@
+export default {
+    pinnedPackages: [
+        'typescript',
+        {
+            name: 'typescript',
+            reason: 'Typescript 5 and above is not compatible with react-scripts',
+        },
+    ],
+};
